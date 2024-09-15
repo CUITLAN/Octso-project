8def36d14e1fb3be6dbbb4c4cc48ba0d6e3205ae
CUITLAN <alan78707@gmail.com>
2024-09-14 18:39:49 -0600

    Se agrego las configuraciones necesarias para el orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmpleoyesModule } from './empleoyes/empleoyes.module';
 import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [EmpleoyesModule, ProductsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'Lacontraseña',
+      database: process.env.name,
+      entities:[],
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    EmpleoyesModule, ProductsModule],
+  
   controllers: [AppController],
   providers: [AppService],
 })
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,17 @@
-export class Product {}
+import { Entity, ChangeStreamCollModDocument, PrimaryGeneratedColumn, Column} from "typeorm";
+
+
+@Entity()
+
+export class Product {
+    @PrimaryGeneratedColumn("uuid")
+    productId: string;
+    @Column({type: "text"})
+    productName: string;
+    @Column({type: "float"})
+    price: number;
+    @Column({type: "int"})
+    countSeal: number;
+    @Column({type: "varchar"})
+    provider: string
+}
