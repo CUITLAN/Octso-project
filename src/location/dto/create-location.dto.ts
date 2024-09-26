e0734d8e40df3f70d50f514750df4f64ef416212
Cuitlan <alan78707@gmail.com>
2024-09-26 15:44:32 -0600

    Added Initial commit Locations

--- src/location/dto/create-location.dto.ts
+++ src/location/dto/create-location.dto.ts
@@ -1,1 +1,13 @@
-export class CreateLocationDto {}
+import { ArrayNotEmpty, IsArray, IsString, MaxLength } from "class-validator";
+import { Location } from "../entities/location.entity";
+export class CreateLocationDto extends Location {
+    @MaxLength(35)
+    @IsString()
+    locationName:string;
+    @IsString()
+    @MaxLength(160)
+    locationAddres:string;
+    @IsArray()
+    @ArrayNotEmpty()
+    locationLat:number[]; 
+}
